8c20cad1e19c02047e11bc0f786ae633792c13ad
[dyad] <yanuaran8@gmail.com>
2025-10-31 16:05:23 +0700

    [dyad] wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SlowMo } from "gsap/EasePack";
+    
+import { Draggable } from "gsap/Draggable";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother"; // ScrollSmoother requires ScrollTrigger
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { TextPlugin } from "gsap/TextPlugin";
+
+gsap.registerPlugin(useGSAP, Draggable, MotionPathHelper, MotionPathPlugin, ScrollTrigger, ScrollSmoother, ScrollToPlugin, TextPlugin, SlowMo);
+
 createRoot(document.getElementById("root")!).render(<App />);
