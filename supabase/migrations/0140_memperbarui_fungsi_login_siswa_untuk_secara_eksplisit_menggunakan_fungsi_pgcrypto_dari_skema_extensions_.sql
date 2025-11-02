87b72ccd8085a7366bc689012d453acd391f4a73
[dyad] <yanuaran8@gmail.com>
2025-11-02 19:27:51 +0700

    [dyad] Memperbaiki panggilan fungsi pgcrypto di fungsi login dan update password siswa. - wrote 2 file(s), executed 2 SQL queries

---
+++ supabase/migrations/0140_memperbarui_fungsi_login_siswa_untuk_secara_eksplisit_menggunakan_fungsi_pgcrypto_dari_skema_extensions_.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION public.login_siswa(id_nis_param text, password_param text)
+ RETURNS SETOF siswa
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO 'public', 'pg_temp', 'extensions'
+AS $function$
+BEGIN
+  -- Set app.current_nis for RLS policies that might be active even within SECURITY DEFINER functions
+  PERFORM set_config('app.current_nis', id_nis_param, true); -- Changed from false to true for session persistence
+
+  RETURN QUERY
+  SELECT
+    s.*
+  FROM public.siswa s
+  WHERE
+    s.id_nis = id_nis_param AND s.password = extensions.crypt(password_param, s.password);
+END;
+$function$;
