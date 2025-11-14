f24ff3322d06a710349c2e053c17932d3c03135d
[dyad] <yanuaran8@gmail.com>
2025-11-15 00:06:02 +0700

    [dyad] Integrating Vercel Speed Insights - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import LandingPage from "./pages/LandingPage";
@@ -72,6 +73,7 @@ const App = () => (
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
+            <SpeedInsights />
           </AdminAuthProvider>
         </StudentAuthProvider>
       </BrowserRouter>
